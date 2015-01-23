98d77c6f7eaccbc9e749dfedb78934de009f151a
Abir Majumdar <abir@layer.com>
2015-01-22 16:40:48 -0800

    Adding Go Example

---
+++ identity-services-samples/go/README.md
@@ -0,0 +1,24 @@
+
+The code in this folder represent a simple backend to generate identity tokens using Go.  
+
+## Example Usage:
+
+###Parameters:
+
+* user_id:  The user ID of the person you want to authenticate
+* nonce: The nonce you receive from Layer. See [docs](https://developer.layer.com/docs/guide#authentication) for more info
+
+The example refers to  "<PROVIDER_ID>", "<KEY_ID>", and "<RSA_PRIVATE_KEY>".  These values are provided by  the Layer Dashboard.
+
+###Output:
+A JSON object containing the identity_token.
+
+Example Result:
+
+```json
+{
+    "identity_token": "eyJ0eXAiOiJKV1MiLCJhbGciOiJSUzI1NiIsImN0eSI6ImxheWVyLWVpdDt2PTEiLCJraWQiOiI2OWZkZDVhYS02NDc4LTExZTQtOTdmMS0xZGVkMDAwMDAwZTYifQ.eyJpc3MiOiI1YTczMWE0Yy02M2JlLTExZTQtOTEyNC1hYWE1MDIwMDc1ZjgiLCJpYXQiOjE0MTUxNTExMTcsImV4cCI6MTQxNTE2MTExNywibmNlIjoibkcrWHZFb0c3dDBXSEZrZjN0QmlWdEdjekFsTXArUmwydXVqWkN0TVJsSEcxb1FVU05BSXZnM0ZTbnFzTDhiNlFFK2pIZU8vZHJsZ2FJNXJnRXVIR2c9PSJ9.SPVPzzN7S09OafQZV7E_LvHF1mmvj5VU0Kn780ef4tegwLUS_pYj7ODfgSZPS2-MNRFhYb5ACZqtoxNkv32CJBzJzFuwBkZ3CsuX8xdpeXWEqvYtK2OV73x1TNA8RGmWyVjVKq7xjpGUORkLk7KQW3QRrQGSVx_jeOiUxb9HvZI"
+}
+```
+
+The layer.go code was a joint effort between [Abir Majumdar](https://github.com/maju6406) and [Emil Sjölander](https://gist.github.com/emilsjolander)
---
+++ identity-services-samples/go/layer.go
@@ -0,0 +1,68 @@
+//Code created by http://github.com/maju6406 and http://github.com/emilsjolander
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+const LayerProviderId = "<PROVIDER_ID>"
+const LayerKeyId = "<KEY_ID>"
+const LayerPrivateKey = "<RSA_PRIVATE_KEY>"
+
+func init() {
+	r := pat.New()
+	r.Post("/authenticate", http.HandlerFunc(authenticate))
+	http.Handle("/", r)
+}
+
+func authenticate(w http.ResponseWriter, r *http.Request) {
+	params := struct {
+		Username string
+		Nonce    string
+	}{}
+	if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
+		writeError(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	token := jwt.New(jwt.GetSigningMethod("RS256"))
+
+	// Set header values
+	token.Header["typ"] = "JWS"
+	token.Header["alg"] = "RS256"
+	token.Header["cty"] = "layer-eit;v=1"
+	token.Header["kid"] = LayerKeyId
+
+	// Set claims
+	token.Claims["iss"] = LayerProviderId
+	token.Claims["prn"] = params.Username
+	token.Claims["iat"] = time.Now().Unix()
+	token.Claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+	token.Claims["nce"] = params.Nonce
+
+	// Sign and get the complete encoded token as a string
+	tokenString, err := token.SignedString([]byte(LayerPrivateKey))
+	if err != nil {
+		writeError(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(&struct {
+		IdentityToken string `json:"identity_token"`
+	}{tokenString})
+}
+
+func writeError(w http.ResponseWriter, e error, status int) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(&struct {
+		Error  string `json:"error"`
+		Status int    `json:"status"`
+	}{e.Error(), status})
+}
