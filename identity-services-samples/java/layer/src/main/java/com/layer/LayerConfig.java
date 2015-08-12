08916ff522203c58c1b302e0d360a2c6d67e19f8
Sean Cook <sean@layer.com>
2015-08-11 22:48:18 -0700

    Removes test constants from config

--- identity-services-samples/java/layer/src/main/java/com/layer/LayerConfig.java
+++ identity-services-samples/java/layer/src/main/java/com/layer/LayerConfig.java
@@ -6,12 +6,9 @@ package com.layer;
  * All values are available in the Layer Dashboard for your project.
  */
 public class LayerConfig {
-    private final static String LAYER_KEY_ID =
-            "layer:///keys/e3b73796-3a1b-11e5-b8be-fdebe7056b10";
-    private final static String LAYER_PROVIDER_ID =
-            "layer:///providers/251c5b9c-cc41-11e4-9872-eae8a6002c37";
-    private final static String LAYER_RSA_KEY_PATH =
-            "/Users/sean/Desktop/layer.pk8";
+    private final static String LAYER_KEY_ID = "";
+    private final static String LAYER_PROVIDER_ID = "";
+    private final static String LAYER_RSA_KEY_PATH = "";
 
     public String getProviderId() {
         return LAYER_PROVIDER_ID;
